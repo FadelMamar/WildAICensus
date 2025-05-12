8b2f78dc859e566470ca5f56af35793d5b571c0f
fadel <fadel.seydou@gmail.com>
2025-05-12 20:42:51 +0200

    adding detections postprocessros

--- src/datalabeling/common/processor.py
+++ src/datalabeling/common/processor.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import torch
+import albumentations as A
 
 
 class FeatureExtractor:
@@ -24,14 +25,68 @@ class FeatureExtractor:
         return features
 
 
-from PIL import Image
+class Classifier(object):
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        label_map: dict,
+        feature_extractor: FeatureExtractor = None,
+        imgsz: int = 96,
+        device: str = "cpu",
+    ):
+        self.model = model
+        self.label_map = label_map
+
+        self.device = device
+        self.model = self.model.to(self.device)
+
+        self.feature_extractor = feature_extractor
+
+        self.transform = A.Compose(
+            [
+                A.PadIfNeeded(min_height=imgsz, min_width=imgsz),
+                # A.CenterCrop(height=imgsz,width=imgsz,pad_if_needed=True)
+            ]
+        )
+
+    def run(self, images: list[np.ndarray]):
+        preprocessed = [self.transform(image=image)["image"] for image in images]
+
+        if self.feature_extractor:
+            preprocessed = [
+                self.feature_extractor.get_features(image) for image in preprocessed
+            ]
+            preprocessed = torch.Tensor(np.stack(preprocessed)).to(self.device)
+
+        with torch.no_grad():
+            probs = self.model(preprocessed).softmax(1)
+            pred = probs.argmax(1).cpu().long().flatten().to_list()
+
+        pred = list(map(lambda x: self.label_map[x], pred))
+
+        return pred
 
 
 # TODO
-def Postprocessor(object):
-    def __init__(self, classifier):
+class DetectionsPostprocessor(object):
+    def __init__(self, classifier: Classifier, keep_classes: str = ["groundtruth"]):
         self.classifier = classifier
+        self.keep = keep_classes
 
     # TODO: implement
-    def run(self, detections: list[dict], image: Image.Image):
-        pass
+    def run(self, detections: list[dict], image: np.ndarray) -> list[dict]:
+        dets = []
+
+        for det in detections:
+            x1 = det["x_min"]
+            y1 = det["y_min"]
+            x2 = det["x_max"]
+            y2 = det["y_max"]
+
+            dets.append(image[y1:y2, x1:x2])
+
+        preds = self.classifier.run(dets)
+
+        out = [det for i, det in enumerate(detections) if preds[i] in self.keep]
+
+        return out
