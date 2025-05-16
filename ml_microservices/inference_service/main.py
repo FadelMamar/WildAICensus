317efb767adcd840db32b0a33e11739840ba2fbd
fadel <fadel.seydou@gmail.com>
2025-05-17 01:36:16 +0200

    update

--- ml_microservices/inference_service/main.py
+++ ml_microservices/inference_service/main.py
@@ -5,6 +5,6 @@ import os
 if __name__ == "__main__":
     run_inference_server(
         port=os.environ.get("INFERENCE_PORT", 4141),
-        workers_per_device=os.environ.get("NUM_WORKERS", 2),
+        workers_per_device=int(os.environ.get("NUM_WORKERS", 2)),
         max_batch_size=1,
     )
