d4f23e16693c72d8244e4b6bffead6b33df9496c
fadel <fadel.seydou@gmail.com>
2025-06-02 17:06:34 +0200

    update

--- helper-scripts/register_run_to_mlflow.bat
+++ helper-scripts/register_run_to_mlflow.bat
@@ -2,7 +2,9 @@ call cd "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling"
 
 call deactivate
 
-call helper-scripts\activate_label-backend_env.bat
+@REM call helper-scripts\activate_label-backend_env.bat
+
+call .venv-export\Scripts\activate
 
 @REM --use-sliding-window adding this flag will enabled sahi inference
 
@@ -10,15 +12,11 @@ call set MLFLOW_S3_ENDPOINT_URL=http://localhost:9000
 call set AWS_SECRET_ACCESS_KEY=minioadmin
 call set AWS_ACCESS_KEY_ID=minioadmin
 
-@REM call uv run tools\register_model.py --exp-name "labeler" --model "D:\datalabeling\base_models_weights\best.pt"^
-@REM         --model-name "labeler" --mlflow-tracking-uri "http://localhost:5000"^
-@REM         --tilesize 800 --imgsz 800 --confidence-threshold 0.1 --overlap-ratio 0.15^
-@REM         --use-sliding-window
+call python tools\register_model.py register_detector "runs/mlflow/140168774036374062/f5b7124be14c4c89b8edd26bcf7a9a76/artifacts/weights/best.pt"^
+        "labeler" "engine" 960 1 "cuda:0" "http://localhost:5000" "False" "detect"
 
-@REM call uv run tools\register_model.py --exp-name "labeler" --model ""^
-@REM         --model-name "detector" --mlflow-tracking-uri "http://localhost:5000"^
-@REM         --tilesize 800 --imgsz 800 --confidence-threshold 0.1 --overlap-ratio 0.15^
-@REM         --use-sliding-window
+
+
 
 
 call deactivate
