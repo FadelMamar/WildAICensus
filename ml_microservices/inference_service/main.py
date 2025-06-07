a71f4ff98f5ce22fbf66f79c60fa436b7adef004
fadel <fadel.seydou@gmail.com>
2025-06-07 13:06:11 +0200

    update

--- ml_microservices/inference_service/main.py
+++ ml_microservices/inference_service/main.py
@@ -1,10 +1,10 @@
-from inference_service.endpoints import run_inference_server
+from src.inference_service.endpoints import run_inference_server
 import os
 
 
 if __name__ == "__main__":
     run_inference_server(
-        port=os.environ.get("INFERENCE_PORT", 4141),
+        port=int(os.environ.get("INFERENCE_PORT", 4141)),
         workers_per_device=int(os.environ.get("NUM_WORKERS", 1)),
-        max_batch_size=1,
+        max_batch_size=int(os.environ.get("BATCH_SIZE", 1)),
     )
--- ml_microservices/inference_service/src/inference_service/endpoints.py
+++ ml_microservices/inference_service/src/inference_service/endpoints.py
@@ -1,10 +1,20 @@
 from .predictor import MyModelAPI
 import litserve as ls
+import torch
 
 
-def run_inference_server(port=4141, max_batch_size=1, workers_per_device=2):
-    api = MyModelAPI()
+
+def run_inference_server(port=4141, max_batch_size=1, workers_per_device=1):
+    api = MyModelAPI(max_batch_size=max_batch_size)
+
+    device = "cuda" if torch.cuda.is_available() else "auto"
+
     server = ls.LitServer(
-        api, max_batch_size=max_batch_size, workers_per_device=workers_per_device
+        api, 
+        max_batch_size=max_batch_size, 
+        workers_per_device=workers_per_device,
+        accelerator=device
     )
-    server.run(port=port)
+    server.run(port=port,
+                generate_client_file=True
+                )
