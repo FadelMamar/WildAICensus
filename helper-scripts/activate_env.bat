74ee79934d1c154794660c375426807538f4c6ac
fadel <fadel.seydou@gmail.com>
2025-06-17 14:37:21 +0200

    rename

--- helper-scripts/activate_env.bat
+++ helper-scripts/activate_env.bat
@@ -4,3 +4,7 @@
 call "C:\Users\Machine Learning\Desktop\workspace-wildAI\datalabeling\.venv\Scripts\activate"
 
 call "D:\datalabeling\.venv\Scripts\activate"
+
+call set LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT=D:\
+call set LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=true
+call set LOCAL_FILES_DOCUMENT_ROOT=D:\
